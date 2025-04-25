8c514adc1cd6b45d4505e9e2eeb7813a96bbd3cd
river <riveronly@outlook.com>
2025-04-25 22:45:54 +0800

    update

--- app/src/main/kotlin/com/riveronly/wanandroid/ui/theme/Color.kt
+++ app/src/main/kotlin/com/riveronly/wanandroid/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val blue = Color(0xFF81D8d0) //蒂芙尼蓝
+val blue_deep = Color(0xFF002FA7) //克莱因蓝
+val yellow = Color(0xFFF7E14D) //申布伦黄
+val green = Color(0xFF008C8C) //马尔斯绿
